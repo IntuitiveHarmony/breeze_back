d99f60c315bac39fd8114416143834f28e09b653
ununique3 <j.horst77@gmail.com>
2022-09-20 20:05:22 -0600

    added /

--- breeze_api/urls.py
+++ breeze_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/sequences', views.SequenceList.as_view(), name='sequence_list'),
+    path('api/sequences/', views.SequenceList.as_view(), name='sequence_list'),
     path('api/sequences/<int:pk>', views.SequenceDetail.as_view(), name='sequence_detail'),
 ]
