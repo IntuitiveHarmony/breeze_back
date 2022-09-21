9e108c3037156182ee9e460ff7ef117358ca4471
ampersand22 <jeyulmer@gmail.com>
2022-09-21 11:15:56 -0500

    first

--- breeze_api/models.py
+++ breeze_api/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Sequence(models.Model):
     name = models.CharField(max_length=32)
     tempo = models.IntegerField()
+
+class Users(models.Model)
+    email = models.EmailField(max_length=75, unique=True)
+    password = models.CharField(max_length=1000)
+    username = models.CharField(max_length=32, unique=True)
