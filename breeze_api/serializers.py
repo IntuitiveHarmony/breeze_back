e35bf1d711bab475efd6ab1c0249c3b65f654323
ununique3 <j.horst77@gmail.com>
2022-09-27 10:45:01 -0600

    updated model for 1st polybeast track

--- breeze_api/serializers.py
+++ breeze_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Sequence
 class SequenceSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Sequence # tell django which model to use
-        fields = ('id', 'name', 'tempo',) # tell django which fields to includeSequence
+        fields = ('id', 'name', 'tempo', 'polyCsSteps', 'polyCsSynth', 'polyCsVolume', 'polyCsFilter', 'polyCsDist', 'polyCsReverb', 'polyCsDelay',) # tell django which fields to includeSequence
