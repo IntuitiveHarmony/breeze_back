ffb863864d8d8b9fe72cc1251fc2147615bbe911
ununique3 <j.horst77@gmail.com>
2022-09-28 12:50:23 -0600

    updated models for 3 polys

--- breeze_api/serializers.py
+++ breeze_api/serializers.py
@@ -4,4 +4,31 @@ from .models import Sequence
 class SequenceSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Sequence # tell django which model to use
-        fields = ('id', 'name', 'tempo', 'polyCsSteps', 'polyCsSynth', 'polyCsVolume', 'polyCsFilter', 'polyCsDist', 'polyCsReverb', 'polyCsDelay',) # tell django which fields to includeSequence
+        fields = (
+            'id',
+            'name',
+            'tempo',
+            
+            'poly0Steps',
+            'poly0Synth',
+            'poly0Volume',
+            'poly0Filter',
+            'poly0Dist',
+            'poly0Reverb',
+            'poly0Delay',
+
+            'poly1Steps',
+            'poly1Synth',
+            'poly1Volume',
+            'poly1Filter',
+            'poly1Dist',
+            'poly1Reverb',
+            'poly1Delay',
+
+            'poly2Steps',
+            'poly2Synth',
+            'poly2Volume',
+            'poly2Filter',
+            'poly2Dist',
+            'poly2Reverb',
+            'poly2Delay',) # tell django which fields to includeSequence
