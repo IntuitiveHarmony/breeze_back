169a9ceda6f777987b5c9b0d94732af154de0bd8
ampersand22 <jeyulmer@gmail.com>
2022-09-29 11:22:17 -0500

    try again

--- breeze_back/settings.py
+++ breeze_back/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['localhost', 'breeze-back.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'bcryptjs',
+    'bcrypt',
     'corsheaders',
     'rest_framework',
     'breeze_api',
